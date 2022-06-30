d0b4b1866e537be5d6ce7dd6e3f1d61ebd4d4e36
Andrew Baxter <asbaxter1@gmail.com>
2022-06-30 18:06:13 -0400

    first API fetch takes user input and grabs coordinates, then run another api fetch to grab weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,34 @@ const inputValue = document.querySelector(".input-value");
 function buttonClickHandle(event){
     event.preventDefault();
     fetch('http://api.openweathermap.org/geo/1.0/direct?q='+ inputValue.value +'&limit=1&appid=dd2013fa50ff58801aff47ba696348c0')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    .then(function(response) {
+        if (response.ok) {
+          response.json().then(function(data) {
+            runLatLon(data);
+          });
+        } else {
+          alert('City Not Found');
+        }
+    });
 
-    .catch(error => alert("wrong city name!"))
+};
 
+function runLatLon(data) {
+    let lat = data[0].lat; 
+    let lon = data[0].lon;
+    console.log(lat);
+    console.log(lon);
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ lon +'&appid=dd2013fa50ff58801aff47ba696348c0&units=imperial')
+    
+    .then(function(response) {
+        if (response.ok) {
+          response.json().then(function(data) {
+            console.log(data)
+          });
+        } else {
+          alert('lat lon not working');
+        }
+    });
 };
 
 
