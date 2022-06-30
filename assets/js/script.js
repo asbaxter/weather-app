4186380318f506febaabafb070262ebd607d2ebc
Andrew Baxter <asbaxter1@gmail.com>
2022-06-30 19:01:33 -0400

    webpage displays current weather conditions for user's input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 const searchForm = document.querySelector(".search-form");
 const inputValue = document.querySelector(".input-value");
+const currentDay = document.querySelector("#current-day")
 
 
-
-function buttonClickHandle(event){
+  // on button click the user input is passed into the API, API returns data into the runLatLon function
+function buttonClickHandler(event){
     event.preventDefault();
     fetch('http://api.openweathermap.org/geo/1.0/direct?q='+ inputValue.value +'&limit=1&appid=dd2013fa50ff58801aff47ba696348c0')
     .then(function(response) {
@@ -12,29 +13,57 @@ function buttonClickHandle(event){
             runLatLon(data);
           });
         } else {
-          alert('City Not Found');
+          alert('City not found');
         }
     });
 
 };
 
+// after latitude and longitude have been obtained in the buttonClickHandler API fetches weather data for that location
 function runLatLon(data) {
     let lat = data[0].lat; 
     let lon = data[0].lon;
-    console.log(lat);
-    console.log(lon);
+    let cityName = data[0].name;
     fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ lon +'&appid=dd2013fa50ff58801aff47ba696348c0&units=imperial')
     
     .then(function(response) {
         if (response.ok) {
           response.json().then(function(data) {
-            console.log(data)
+            displayWeatherData(data, cityName);
           });
         } else {
-          alert('lat lon not working');
+          alert('something went wrong verifying latitude and longitude');
         }
     });
 };
 
+function displayWeatherData(data, cityName){
+  let currentTemp = data.current.temp;
+  let currentWind = data.current.wind_speed;
+  let currentHumidity = data.current.humidity;
+  let currentUVI = data.current.uvi;
 
-searchForm.addEventListener("submit", buttonClickHandle);
+  let displayCity = document.createElement("h2");
+  let displayTemp = document.createElement("p");
+  let displayWind = document.createElement("p");
+  let displayHumidity = document.createElement("p");
+  let displayUVI = document.createElement("p");
+ 
+
+  displayCity.textContent = cityName;
+  displayTemp.textContent = "Temp: " + currentTemp;
+  displayWind.textContent = "Wind: " + currentWind + " MPH";
+  displayHumidity.textContent = "Humidity " + currentHumidity + "%";
+  displayUVI.textContent = "UV Index " + currentUVI;
+
+
+  currentDay.append(displayCity);
+  currentDay.append(displayTemp);
+  currentDay.append(displayWind);
+  currentDay.append(displayHumidity);
+  currentDay.append(displayUVI);
+
+};
+
+
+searchForm.addEventListener("submit", buttonClickHandler);
